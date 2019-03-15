a1ccbdf8e02c9addb8eb0e85adeb6e6606bed19d
Morrie <morriedog@hotmail.com>
2019-03-15 18:37:24 +0800

    finish task CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,53 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    find_task
+  end
+
+  def new
+    @task = Task.new    
+  end
+  
+  def create
+    @task = Task.new( task_params )
+    if @task.save
+      redirect_to tasks_path, notice: "任務新增成功!"
+    else
+      render :new
+    end
+  end
+  
+
+  def edit
+    find_task
+  end
+
+  def update
+    find_task
+    @task.update(task_params)
+    redirect_to tasks_path, notice: "任務更新成功!"
+  end
+  
+
+  def destroy
+    find_task
+    @task.destroy if @task
+    redirect_to tasks_path, notice: "任務已刪除!"
+  end
+
+  private
+  def find_task
+    @task = Task.find_by_id(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content, :status, :priority, :finish_time)
+  end
+  
+  
+
 end
