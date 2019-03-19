3645c292dc2df5dcbfabe5bb6018e036df7bf5d1
Morrie <morriedog@hotmail.com>
2019-03-19 11:46:25 +0800

    order_by_finish_time to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates_presence_of :title, :content
   default_scope { order(created_at: :desc) }
+  scope :order_by_finish_time, -> { order(finish_time: :desc) }
 end
